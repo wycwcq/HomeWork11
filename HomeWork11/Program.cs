fc1a44ac4dd0ba6e64c67cd1a437749fa9f21e19
wycwcq <1193032407@qq.com>
2020-05-30 14:45:44 +0800

    添加项目文件。

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var db = new OrderContext())
+                {
+                    var order = db.OrderItems.FirstOrDefault();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
